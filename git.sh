8774a874b213749d223e2c1c1198be34873c6a2c
sw586 <sw586@126.com>
2020-08-03 02:27:20 +0800

    2020-08-03_02-27-20

--- git.sh
+++ git.sh
@@ -2,10 +2,9 @@
 #git clone git@github.com:sw586/SWCF-ddns-Actions
 #git config --global user.email "sw586@126.com"
 #git config --global user.name "sw586"
-#git remote add origin git@github.com:sw586/uploadfile.git
 #git init
-#Linux下免密一键提交
-#1、在~/下， touch .git-credentials，在文件中输入：https://{username}:{password}[@github.com
+#git remote add origin git@github.com:sw586/uploadfile.git
+#ssh-keygen -t rsa -C "sw586@126.com" //一直回车
+#打开：/root/.ssh/id_rsa.pub 复制到https://github.com/settings/keys ----SSH keys
 #2、执行git config --global credential.helper store
-#3、之后cd到项目目录，执行git pull命令，会提示输入账号密码。输完这一次以后就不再需要
 git add .  &&  git commit -m "`date "+%Y-%m-%d_%H-%M-%S"`"  &&  git push -u origin +master
