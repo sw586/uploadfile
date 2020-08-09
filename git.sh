064970cd69dde18c10efc7fbb685c0e4bdf4f2d4
sw586@126.com <sw586>
2020-08-09 15:18:41 +0800

    2020-08-09_15-18-41

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-cd /www/wwwroot/2ht.cn/uploadfile  &&  git add .  &&  git commit -m "`date "+%Y-%m-%d_%H-%M-%S"`"  &&  git push -u origin +master  &&  curl "http://2ht.cn/index.php?s=githubup&c=home&m=index"
+cd /www/wwwroot/2ht.cn/uploadfile  &&  git config --system user.email "sw586"  &&  git config --system user.name "sw586@126.com"  &&  git add .  &&  git commit -m "`date "+%Y-%m-%d_%H-%M-%S"`"  &&  git push -u origin +master  &&  curl "http://2ht.cn/index.php?s=githubup&c=home&m=index"
